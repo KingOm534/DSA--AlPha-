7e077c47ecaf592eb83afa6de2d8b684ee5a0c46
KingOm534 <omkarrlande.534@gmail.com>
2023-03-06 23:10:06 +0530

    day3

---
+++ Functions/PrimeInRange.java
@@ -0,0 +1,24 @@
+package Functions;
+
+public class PrimeInRange {
+
+    public static boolean IsPrime(int n) {
+        if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i >= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int PrimesInRange(int n) {
+        return 3;
+    }
+
+    public static void main(String[] args) {
+        IsPrime(6);
+    }
+}
